a8794a91911099b8bb7df37f6ba8973c632de9e3
Angela Jovin <67833415+AngelaJovin@users.noreply.github.com>
2021-09-25 20:27:41 +0300

    Add files via upload

--- unimap website/js/scripts.js
+++ unimap website/js/scripts.js
@@ -1,7 +1,17 @@
-/*!
-* Start Bootstrap - Small Business v5.0.3 (https://startbootstrap.com/template/small-business)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+//the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
